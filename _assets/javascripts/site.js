3cbc05eab442c214eee7b308e313fe0a53adc5a9
myke <michael@evoluted.net>
2015-08-12 12:32:38 +0100

    .

--- _assets/javascripts/site.js
+++ _assets/javascripts/site.js
@@ -1,4 +1,30 @@
+var cursorTimeout, cursorStates, cursorStatesIndex;
 
 $(function() {
-	console.log("Let's do these!");
+	console.log("Let's do this!");
+
+	cursorStates = [
+		"n-resize",
+		"ne-resize",
+		"e-resize",
+		"se-resize",
+		"s-resize",
+		"sw-resize",
+		"w-resize",
+		"nw-resize"
+	];
+	cursorStatesIndex = 0;
+	$('.header__logo').hover(function() {
+		cursorTimeout = setInterval(function() {
+			$('.header__logo').css('cursor', cursorStates[cursorStatesIndex]);
+			//console.log(cursorStates[cursorStatesIndex]);
+			cursorStatesIndex++;
+			if (cursorStatesIndex >= cursorStates.length) cursorStatesIndex = 0;
+		}, 60);
+	}, function() {
+		$('.header__logo').css('cursor', 'auto');
+		clearInterval(cursorTimeout);
+	})
+
+
 });
